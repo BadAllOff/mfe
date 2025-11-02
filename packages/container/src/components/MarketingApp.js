8eb2597a268c5f4ea57d59866a50d30add4d3eec
shamil <sbadalov@griddynamics.com>
2025-11-02 21:06:43 +0100

    Refactor MarketingApp to simplify navigation handling and improve useEffect structure for better readability

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,34 +3,36 @@ import { mount } from 'marketing/MarketingApp';
 import { useHistory } from 'react-router-dom';
 
 export default function MarketingApp() {
-    const marketingRef = useRef(null);
-    const history = useHistory();
-    let onParentNav = null;
-    
-    useEffect(() => {
-        // if (marketingRef.current) {
-        onParentNav = mount(marketingRef.current, {
-            onNavigate: ({ pathname: nextPathname }) => {
-                const { pathname } = history.location;
+  const marketingRef = useRef(null);
+  const history = useHistory();
 
-                if (pathname !== nextPathname) {
-                    history.push(nextPathname);
-                }
-            }
-        });
-        
-        if (onParentNav.onParentNavigate) {
-            history.listen(onParentNav.onParentNavigate);
-        } else {
-            console.error('onParentNavigate is not a function');
+  useEffect(() => {
+    if (marketingRef.current) {
+      const { onParentNavigate } = mount(marketingRef.current, {
+        onNavigate: ({ pathname: nextPathname }) => {
+          const { pathname } = history.location;
+
+          if (pathname !== nextPathname) {
+            history.push(nextPathname);
+          }
         }
-        // }
-        return () => {
-            if (marketingRef.current) {
-                marketingRef.current.unmount();
-            }
-        };
-    }, [onParentNav]);
+      });
 
-    return <div ref={marketingRef} />;
+
+      // if (onParentNavigate) {
+      //     history.listen(onParentNavigate);
+      // } else {
+      //     console.error('onParentNavigate is not a function');
+      // }
+      // }
+    };
+
+    return () => {
+      if (marketingRef.current) {
+        marketingRef.current.unmount();
+      }
+    };
+  }, []);
+
+  return <div ref={marketingRef} />;
 }
