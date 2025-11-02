c56009f89aa4e8ae0ad615469c13102941ea1009
shamil <sbadalov@griddynamics.com>
2025-11-02 19:36:15 +0100

    Implement routing synchronization between MarketingApp and history, enhancing navigation handling in microfrontend architecture

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,20 @@
 import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 export default function MarketingApp() {
     const marketingRef = useRef(null);
-
+    const history = useHistory();
     useEffect(() => {
         if (marketingRef.current) {
-            mount(marketingRef.current);
+            const {onParentNavigate} = mount(marketingRef.current, { onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }});
+            
+            history.listen(onParentNavigate);
         }
         return () => {
             if (marketingRef.current) {
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-export default function App() {
+export default function App({ history }) {
     const generateClassName = createGenerateClassName({
         productionPrefix: 'ma',
     });
 
     return (
-        <BrowserRouter>
-            <StylesProvider generateClassName={generateClassName}>
+        <StylesProvider generateClassName={generateClassName}>
+            <Router history={history}>
                 <Switch>
                     <Route path="/pricing" component={Pricing} />
                     <Route path="/" component={Landing} />
                 </Switch>
-            </StylesProvider>
-        </BrowserRouter>
+            </Router>
+        </StylesProvider>
     );
 }
