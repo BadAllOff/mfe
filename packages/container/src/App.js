c7bae3c7219ff723a071ddfb0b739e45a8094ef4
shamil <sbadalov@griddynamics.com>
2025-11-04 20:15:20 +0100

    Refactor App component to use React.lazy for dynamic imports of MarketingApp and AuthApp, and add Progress component for loading indication during lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/Auth';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/Auth'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -14,10 +16,12 @@ export default function App() {
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <Header />
-                <Switch>
-                    <Route path="/auth" component={AuthApp} />
-                    <Route path="/" component={MarketingApp} />
-                </Switch>
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth" component={AuthLazy} />
+                        <Route path="/" component={MarketingLazy} />
+                    </Switch>
+                </Suspense>
             </StylesProvider>
         </BrowserRouter>
     );
